b15c59a636320a9dc57b24851248eb3978c0ad9a
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 17:32:03 +0800

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,4 +1,7 @@
+mysql -h127.0.0.1 -P3306 -uopenshift -ppassword
+use demo;
+
 drop table AB;
 create table AB (AppVersion varchar(100), Attitude varchar(10));
-insert into AB values("discount","like");
-insert into AB values("buyonesendone","like");
+insert into AB values("discount","look");
+insert into AB values("buy1get1","buy");
