7a25331eb32b3384ba27c11a766d8a5b8c40e33b
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 18:28:14 +0800

    Delete table.sql

--- table.sql
+++
@@ -1,7 +0,0 @@
-mysql -h127.0.0.1 -P3306 -uopenshift -ppassword
-use demo;
-
-drop table AB;
-create table AB (AppVersion varchar(100), Step varchar(10));
-insert into AB values("discount","look");
-insert into AB values("buy1get1","buy");
