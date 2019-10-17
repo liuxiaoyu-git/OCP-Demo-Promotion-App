121b5d822a7da08b26ad5a44edb22b25453f3d11
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-17 14:53:37 +0800

    Create makedeal.php

---
+++ makedeal.php
@@ -0,0 +1,25 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("DATABASE_USER");
+$dbname = getenv("DATABASE_NAME");
+$dbpwd = getenv("DATABASE_PASSWORD");
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
+$sql = "insert into AB values('discount','like')";
+if ($connection->query($sql) !== TRUE) {  
+    echo "发生数据库操作错误";  
+} 
+mysqli_close($connection);
+?>
+
+<html>
+<div id="box">
+    <table width="100%" height="100%">
+        <tr>
+            <td align="center">
+                <font size="16" color="#FF0000">谢谢你喜欢50%的折扣促销方案！ </font>
+            </td>
+        </tr>
+    </table>
+</div>
+</html>
