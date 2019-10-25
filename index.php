ec7c34279fe973b4383f24a116c9c1a903d45b8c
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-25 23:37:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $rs = $connection->query($sql);
     <table width="100%" height="100%">
         <tr>
             <td align="center">
-                <font color="#FF0000">
+                <font size="3" color="#FF0000">
                     <?php
                     while ($row = mysqli_fetch_assoc($rs)) {
                         echo $row['AppVersion']. "促销方案的用户购买率: ". $row['BuyRate']. "<br>";
